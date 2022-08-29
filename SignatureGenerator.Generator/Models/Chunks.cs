76f9ca5aa1cc4c355a011600973a1c4f0f5a12af
Vladislav Bebchuk <vladislav.bebchuk@monopoly.su>
2022-08-29 12:36:35 +0300

    class for record typo fix

--- SignatureGenerator.Generator/Models/Chunks.cs
+++ SignatureGenerator.Generator/Models/Chunks.cs
@@ -4,16 +4,5 @@ namespace SignatureGenerator.Generator.Models
 {
     public record ByteChunk(int Order, byte[] Bytes) : ISorterable<int>, IByteRepresentationHolder;
 
-    public class HashedChunk : ISorterable<int>
-    {
-        public int Order { get; set; }
-
-        public HashedChunk(int order, string sha256)
-        {
-            Order = order;
-            SHA256 = sha256;
-        }
-
-        public string SHA256 { get; set; }
-    }
+    public record HashedChunk(int Order, string SHA256) : ISorterable<int>;
 }
